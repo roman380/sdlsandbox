99795c42fef3d878cc3810eeeb2d6541acf3dcdb
Roman Ryltsov <roman.ryltsov@castlabs.com>
2023-01-11 00:08:15 +0100

    Minor

--- sandbox.cpp
+++ sandbox.cpp
@@ -19,36 +19,34 @@ int main(int argc, char** argv)
 
 	verify(SDL_Init(SDL_INIT_VIDEO) >= 0);
 
-	SDL_DisplayMode displayMode;
-	SDL_GetDesktopDisplayMode(0, &displayMode);
+	SDL_DisplayMode DisplayMode;
+	SDL_GetDesktopDisplayMode(0, &DisplayMode);
+	std::cout << "Desktop Display Mode: " << DisplayMode.format << ", " << DisplayMode.w << " x " << DisplayMode.h << ", refresh_rate " << DisplayMode.refresh_rate << std::endl;
 
-	const uint32_t windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
-	SDL_Window* Window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, displayMode.w * 0.66, displayMode.h * 0.66, windowFlags);
+	SDL_Window* Window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DisplayMode.w * 0.66, DisplayMode.h * 0.66, SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
 
-	{
-		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-		SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
-		auto CreateContext = [&](int MajorVersion, int MinorVersion)
-		{
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, MajorVersion);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, MinorVersion);
-			SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-			Context = SDL_GL_CreateContext(Window);
-		};
+    auto CreateContext = [&](int MajorVersion, int MinorVersion)
+    {
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, MajorVersion);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, MinorVersion);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+        Context = SDL_GL_CreateContext(Window);
+    };
 
-		assert(!Context);
-		CreateContext(3, 2);
-		assert(!Context);
+    assert(!Context);
+    CreateContext(3, 2);
+    assert(!Context);
 
-		SDL_GL_MakeCurrent(Window, Context);
-		SDL_GL_SetSwapInterval(1);
-	}
+    SDL_GL_MakeCurrent(Window, Context);
+    SDL_GL_SetSwapInterval(1);
 
 	int Width = 0, Height = 0;
 	SDL_GL_GetDrawableSize(Window, &Width, &Height);
